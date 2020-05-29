18940e8871ccd37bf37f1bb1236682590ef12b83
commandblock2 <tony_hu@live.com>
2020-05-29 22:01:06 +0800

    update default value and it is just fucking painful to do this in termux

--- 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/ReachAura.kt
+++ 1.8.9-Forge/src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/ReachAura.kt
@@ -67,7 +67,7 @@ class ReachAura : Module()
 
     private val rangeValue = FloatValue("Range", 20f, 1f, 100f)
     private val tpDistanceValue = FloatValue("TpDistance", 4.0f, 0.5f, 10.0f)
-    private val stopAtDistance = FloatValue("StopAtDistance", 0.0f, 0.0f, 6.0f)
+    private val stopAtDistance = FloatValue("StopAtDistance", 2.0f, 2.0f, 6.0f)
 
     private val pathFindingMode = ListValue("PathFindingMode", arrayOf("Simple",
             "NaiveAstarGround", "NaiveAstarFly"), "NaiveAstarFly")
